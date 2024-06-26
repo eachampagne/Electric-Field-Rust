d6ae1bdaad9496744f5f72f6f5289932a6ebbf34
Elizabeth Champagne <e.a.champagne@outlook.com>
2024-06-25 23:03:37 -0700

    begin work on rewrite of original code (naive) - got two rust files to talk to each other

---
+++ src/bin/naive/main.rs
@@ -0,0 +1,8 @@
+mod vectors_naive;
+
+use crate::vectors_naive::test;
+
+fn main() {
+    println!("Hello, world!");
+    test();
+}
---
+++ src/bin/naive/vectors_naive.rs
@@ -0,0 +1,9 @@
+pub struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+pub fn test() {
+    println!("this is the other file");    
+}
