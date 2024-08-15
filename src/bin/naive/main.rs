a874efbe1f917d33938714aff47d457a930735b2
Elizabeth Champagne <e.a.champagne@outlook.com>
2024-08-14 22:46:47 -0700

    created cartesian and spherical vector structs with methods to print them

--- src/bin/naive/main.rs
+++ src/bin/naive/main.rs
@@ -1,8 +1,17 @@
 mod vectors_naive;
 
-use crate::vectors_naive::test;
+use crate::vectors_naive::*;
 
 fn main() {
     println!("Hello, world!");
     test();
+
+    let vec_test = Vector {x:1.0, y:2.0, z:3.0};
+    vec_test.trace();
+
+    let sphere_test = SphereVector {r:2.0, theta:10.0, phi:20.0};
+    sphere_test.trace();
+
+    println!("{}", vec_test.mag());
+    vec_test.normalize().trace();
 }
--- src/bin/naive/vectors_naive.rs
+++ src/bin/naive/vectors_naive.rs
@@ -1,7 +1,50 @@
-pub struct Vector3 {
-    x: f32,
-    y: f32,
-    z: f32,
+pub struct Vector {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vector {
+    
+    //Commentary:
+    //I don't like the order I defined these functions in, but for the purpose of creating a
+    //line-by-line replica I guess I'll keep it at least in this version
+
+    //TODO: proper constructor
+
+    //Commentary:
+    //I have no idea why I didn't make this edit the vector in place
+    //In Rust, this would work even if the vector isn't mutable
+    //But that wouldn't have been a concern in Python
+    //TODO: fix once I've figured out how constructors work
+    pub fn normalize(&self) -> Vector {
+        let mag = self.mag();
+        return Vector{x:self.x/mag, y:self.y/mag, z:self.z/mag};
+    }
+
+    pub fn trace(&self) {
+        println!("<{0}, {1}, {2}>", self.x, self.y, self.z);
+    }
+
+    //Commentary:
+    //I'm not sure offhand how to do fractional powers in Rust
+    pub fn mag(&self) -> f32 {
+        let magnitude:f32 = f32::sqrt(self.x*self.x+self.y*self.y+self.z*self.z);
+        return magnitude;
+    }
+
+}
+
+pub struct SphereVector {
+    pub r: f32,
+    pub theta: f32,
+    pub phi: f32,
+}
+
+impl SphereVector {
+    pub fn trace(&self) {
+        println!("<{0}, {1}, {2}>", self.r, self.theta, self.phi);
+    }
 }
 
 pub fn test() {
