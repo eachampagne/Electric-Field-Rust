427a3f57d1ca32a07a2e84ac3778446a9246f788
Elizabeth Champagne <e.a.champagne@outlook.com>
2024-08-20 13:12:21 -0700

    added constructor methods for both vector types; privated their fields

--- src/bin/naive/main.rs
+++ src/bin/naive/main.rs
@@ -6,16 +6,16 @@ fn main() {
     println!("Hello, world!");
     test();
 
-    let vec_test = Vector {x:1.0, y:2.0, z:3.0};
+    let vec_test = Vector::new(1.0, 2.0, 3.0);
     vec_test.trace();
 
-    let sphere_test = SphereVector {r:2.0, theta:10.0, phi:20.0};
+    let sphere_test = SphereVector::new(2.0, 10.0, 20.0);
     sphere_test.trace();
 
     println!("{}", vec_test.mag());
     vec_test.normalize().trace();
 
-    let test_2 = Vector {x:4.0, y:6.0, z:8.0};
+    let test_2 = Vector::new(4.0, 6.0, 8.0);
     test_2.trace();
     
     (vec_test + test_2).trace();
--- src/bin/naive/vectors_naive.rs
+++ src/bin/naive/vectors_naive.rs
@@ -2,9 +2,9 @@ use std::ops;
 
 #[derive(Copy, Clone)]
 pub struct Vector {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
 impl Vector {
@@ -13,16 +13,20 @@ impl Vector {
     //I don't like the order I defined these functions in, but for the purpose of creating a
     //line-by-line replica I guess I'll keep it at least in this version
 
-    //TODO: proper constructor
+    //Commentary: I believe that technically, the only true "constructor" is the curly brace
+    //syntax. This is simply a function that acts like a constructor. This does allow me to keep
+    //the fields private however, and I think is slightly easier to write out.
+    pub fn new(x:f32, y:f32, z:f32) -> Vector {
+        return Vector{x:x, y:y, z:z};
+    }
 
     //Commentary:
     //I have no idea why I didn't make this edit the vector in place
     //In Rust, this would work even if the vector isn't mutable
     //But that wouldn't have been a concern in Python
-    //TODO: fix once I've figured out how constructors work
     pub fn normalize(&self) -> Vector {
         let mag = self.mag();
-        return Vector{x:self.x/mag, y:self.y/mag, z:self.z/mag};
+        return Vector::new(self.x/mag, self.y/mag, self.z/mag);
     }
 
     pub fn trace(&self) {
@@ -38,7 +42,6 @@ impl Vector {
 
 }
 
-//Todo: add constructor
 impl ops::Add<Vector> for Vector {
     type Output = Vector;
 
@@ -46,11 +49,10 @@ impl ops::Add<Vector> for Vector {
         let x:f32 = self.x + _rhs.x; 
         let y:f32 = self.y + _rhs.y;
         let z:f32 = self.z + _rhs.z;
-        return Vector{x:x, y:y, z:z};
+        return Vector::new(x, y, z);
     }
 }
 
-//Todo: add constructor
 impl ops::Sub<Vector> for Vector {
     type Output = Vector;
 
@@ -58,12 +60,11 @@ impl ops::Sub<Vector> for Vector {
         let x:f32 = self.x-_rhs.x;
         let y:f32 = self.y-_rhs.y;
         let z:f32 = self.z-_rhs.z;
-        return Vector{x:x, y:y, z:z};
+        return Vector::new(x, y, z);
     }
 }
 
 //Scalar multiplication of form vector * scalar
-//Todo: add constructor
 impl ops::Mul<f32> for Vector {
     type Output = Vector;
 
@@ -71,12 +72,11 @@ impl ops::Mul<f32> for Vector {
         let x:f32 = self.x*_rhs;
         let y:f32 = self.y*_rhs;
         let z:f32 = self.z*_rhs;
-        return Vector{x:x, y:y, z:z};
+        return Vector::new(x, y, z);
     }
 }
 
 //Scalar multiplication of form scalar * vector
-//Todo: add constructor
 impl ops::Mul<Vector> for f32 {
     type Output = Vector;
 
@@ -84,18 +84,22 @@ impl ops::Mul<Vector> for f32 {
         let x:f32 = self*_rhs.x;
         let y:f32 = self*_rhs.y;
         let z:f32 = self*_rhs.z;
-        return Vector{x:x, y:y, z:z};
+        return Vector::new(x, y, z);
     }
 }
 
 #[derive(Copy, Clone)]
 pub struct SphereVector {
-    pub r: f32,
-    pub theta: f32,
-    pub phi: f32,
+    r: f32,
+    theta: f32,
+    phi: f32,
 }
 
 impl SphereVector {
+    pub fn new(r:f32, theta:f32, phi:f32) -> SphereVector {
+        return SphereVector{r:r, theta:theta, phi:phi};
+    }
+
     pub fn trace(&self) {
         println!("<{0}, {1}, {2}>", self.r, self.theta, self.phi);
     }
@@ -103,24 +107,22 @@ impl SphereVector {
 }
 
 //Scalar multiplication of form vector * scalar
-//Todo: add constructor
 impl ops::Mul<f32> for SphereVector {
     type Output = SphereVector;
 
     fn mul(self, _rhs: f32) -> SphereVector {
         let newR:f32 = self.r*_rhs;
-        return SphereVector{r:newR, theta:self.theta, phi:self.phi};
+        return SphereVector::new(newR, self.theta, self.phi);
     }
 }
 
 //Scalar multiplication of form scalar * vector
-//Todo: add constructor
 impl ops::Mul<SphereVector> for f32 {
     type Output = SphereVector;
 
     fn mul(self, _rhs: SphereVector) -> SphereVector {
         let newR:f32 = self*_rhs.r;
-        return SphereVector{r:newR, theta:_rhs.theta, phi:_rhs.phi};
+        return SphereVector::new(newR, _rhs.theta, _rhs.phi);
     }
 }
 
