e8917586a72158576d7bda27157e1c32de15d3e6
Elizabeth Champagne <64558721+eachampagne@users.noreply.github.com>
2024-10-12 23:10:39 -0500

    Implent electric field calculation based on rings

--- src/bin/ring-symmetry/main.rs
+++ src/bin/ring-symmetry/main.rs
@@ -20,18 +20,38 @@ fn main() {
                            //vanishing x and y components
 
         for j in 0..n { //theta
-            let theta = theta1 + j as f64 + 0.5) * dtheta;
+            let theta = theta1 + (j as f64 + 0.5) * dtheta;
 
             //The electric field along the axis of a ring of charge is given as
             //E = (2 * pi * k * lambda * r' * z) / (r'^2+z^2)^(3/2)
-            //where r' is the radius of the ring, z is the height above the ring, and k encodes all
+            //where r' is the radius of the ring, lambda is the linear charge density, 
+            //z is the height above the ring, and k encodes all
             //of the relevant constants (epsilon_naught, etc)
             //(I derived this on paper and checked against the Hyperphysics entry, so I used their
             //conventions)
             //TODO: add link to Hyperphysics
             //
-            //To 
+            //When integrating over a number of rings, the contribution of each ring becomes
+            //dE = (2 * pi * k * r' * z' * sigma * R) / (r'^2 + z'^2)^(3/2) dtheta
+            //where sigma is the area charge density, R is the radius of the sphere (equals 1 in
+            //this program), r' is the radius of each ring, and z' is the difference in z between
+            //the ring and z, the point at which we want to compute the electric field.
+            //
+            //z' and r' are theta dependent.
+            //
+            //r' = sin(theta) and z' = z - cos(theta)
+            //
+            //So the contribution of each ring in terms of theta becomes
+            //dE = (2 * pi * k * sigma * sin(theta)(z-cos(theta))) / (z^2 - 2zcos(theta) + 1) ^ (3/2) dtheta
+            //
+            //TODO: figure out which constants I actually included in the other program
+            //I think I'm at least assuming k = 1
+            
+            let dE = 2.0 * PI * (theta.sin()*(z-theta.cos())) / (z*z-2.0*z*theta.cos()+1.0).powf(1.5) * dtheta;
+            E_z += dE;
         }
+
+        println!("{}", E_z);
 
     }
 }
